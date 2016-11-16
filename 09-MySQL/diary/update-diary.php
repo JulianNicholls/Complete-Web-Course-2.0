b1ebdee05dc616270b5a0bb8342f7b61b0c822df
Julian Nicholls <juliannicholls29@gmail.com>
2016-11-16 14:43:03 +0000

    Sanitise the diary text contents before writing it to the database.

--- 09-MySQL/diary/update-diary.php
+++ 09-MySQL/diary/update-diary.php
@@ -6,7 +6,7 @@
   list($conn, $db_error) = db_connect();
 
   if ($conn) {
-    $query = "UPDATE `users` SET `diary`='" . $_POST['diary'] . "' WHERE `id`=" . $_SESSION['id'];
+    $query = "UPDATE `users` SET `diary`='" . mysqli_real_escape_string($conn, $_POST['diary']) . "' WHERE `id`=" . $_SESSION['id'];
     mysqli_query($conn, $query);
 
     mysqli_close($conn);
