0f9ef9f233faf70ed8b807148581eeccc5803bfc
Julian Nicholls <juliannicholls29@gmail.com>
2018-09-12 09:14:41 +0100

    Load POSTed signup data and check content

--- 14-Twitter-Clone/actions.php
+++ 14-Twitter-Clone/actions.php
@@ -2,6 +2,18 @@
 include ('functions.php');
 
 if ($_GET["action"] == 'login') {
-  $data = getPOSTData();
-  print json_encode($data);
+  $login_data = getPOSTData();
+  $errors = '';
+
+  if (!$login_data->email) {
+    $errors = 'You must enter an email address.';
+  }
+
+  if (strlen($login_data->password) < 6) {
+    $errors .= 'You must enter a password of at least 6 characters.';
+  }
+
+  if ($login_data->loginActive === '0') {
+    echo "Sign user up:" . $errors;
+  }
 }
