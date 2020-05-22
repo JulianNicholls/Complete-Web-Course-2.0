39db00e78718dd06d21e295b3789eebc9c6b5045
Julian Nicholls <juliannicholls29@gmail.com>
2020-05-22 20:39:58 +0100

    Update weather.php for latest Bootstrap

--- 10-APIs/weather.php
+++ 10-APIs/weather.php
@@ -64,13 +64,13 @@ if (isset($_GET['location'])) :
       }
     }
 
-    $result .= "<p>Summary: <strong>{$forecast['weather'][0]['description']}</strong></p>";
-    $result .= "<p>Temperature: <strong>{$forecast['main']['temp']}&deg;C</strong></p>";
+    $result .= "<p><span>Summary:</span> <strong>{$forecast['weather'][0]['description']}</strong></p>";
+    $result .= "<p><span>Temperature:</span> <strong>{$forecast['main']['temp']}&deg;C</strong></p>";
     if ($wind !== '') {
-      $result .= "<p>Wind: <strong>$wind</strong></p>";
+      $result .= "<p><span>Wind:</span> <strong>$wind</strong></p>";
     }
-    $result .= '<p>Sunrise: <strong>' . strftime("%H:%M", $forecast['sys']['sunrise']) . '</strong><br>';
-    $result .= 'Sunset: <strong>' . strftime("%H:%M", $forecast['sys']['sunset']) . '</strong></p>';
+    $result .= '<p><span>Sunrise:</span> <strong>' . strftime("%H:%M", $forecast['sys']['sunrise']) . '</strong><br>';
+    $result .= '<span>Sunset:</span> <strong>' . strftime("%H:%M", $forecast['sys']['sunset']) . '</strong></p>';
   }
   else {
     $result .= "<h5 class=\"my-2 error\">No forecast found for $location.<br>Is it spelled correctly?</h5>";
@@ -88,7 +88,9 @@ endif;
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">    <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+
     <title>Weather Oracle</title>
   </head>
 
@@ -104,7 +106,12 @@ h1, h2, h3, h4, h5, h6 {
 #result {
   background: #5C6BC0;
   color: white;
-  border-radius: 1em;
+  border-radius: 0.5em;
+}
+
+#result span {
+  display: inline-block;
+  width: 7rem;
 }
 
 #result strong {
@@ -126,23 +133,28 @@ h1, h2, h3, h4, h5, h6 {
 
     <div class="container">
       <?php if ($result !== '') : ?>
-        <div class="p-1" id="result">
+        <div class="py-2 px-3 mt-5" id="result">
           <?php echo $result; ?>
         </div>
       <?php endif; ?>
 
-      <div class="mt-3 card card-block">
-        <h4 class="card-title text-sm-center">Enter Location</h4>
-        <form class="form-inline" method="get">
-          <input type="text" class="form-control" id="location" name="location" placeholder="Location, e.g. Bournemouth"/>
-          <button type="submit" class="btn btn-primary">Consult the Oracle</button>
-        </form>
+      <div class="mt-5 card">
+        <div class="card-body">
+          <h4 class="card-title text-sm-center">Enter Location</h4>
+
+          <form class="form-inline" method="get">
+            <input type="text" class="form-control" id="location" name="location" placeholder="Location, e.g. Bournemouth"/>
+            <button type="submit" class="btn btn-primary">Consult the Oracle</button>
+          </form>
+        </div>
       </div>
     </div>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>  
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
   </body>
 </html>
